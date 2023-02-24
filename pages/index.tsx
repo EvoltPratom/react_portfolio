093e980ba7fade521da5134c1262a3a368e9cd66
EvoltPratom <bishantad@gmail.com>
2023-02-24 23:14:03 +0545

    add background

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,25 @@ import Navbar from "../components/Navbar";
 
 
 export default function Home() {
+  const bg = {
+    backgroundImage:
+      "url(https://images.pexels.com/photos/4512439/pexels-photo-4512439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: '100vh',
+    backgroundPosition: "center",
+  };
   return (
     <>
+    <div style={bg}>
       <Navbar>
         <Link href="#home">Home</Link>
         <Link href="#about">About</Link>
         <Link href="#experience">Experience</Link>
         <Link href="#skills">Skills</Link>
       </Navbar>
+      SOmething else
+    </div>
     </>
   )
 }
