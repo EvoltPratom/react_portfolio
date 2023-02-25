420b688ac16409150b72a789e1559743ce27be77
EvoltPratom <bishantad@gmail.com>
2023-02-25 14:52:28 +0545

    add css for text in intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Navbar from "../components/Navbar";
+import styles from "../styles/Home.module.css";
 
 
 export default function Home() {
@@ -8,20 +9,43 @@ export default function Home() {
       "url(https://images.pexels.com/photos/4512439/pexels-photo-4512439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    height: '100vh',
+    height: "100vh",
     backgroundPosition: "center",
   };
   return (
     <>
-    <div style={bg}>
-      <Navbar>
-        <Link href="#home">Home</Link>
-        <Link href="#about">About</Link>
-        <Link href="#experience">Experience</Link>
-        <Link href="#skills">Skills</Link>
-      </Navbar>
-      SOmething else
-    </div>
+      <div style={{ backgroundColor: "black", height: "100vh" }}>
+        <Navbar>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#experience">Experience</Link>
+          <Link href="#skills">Skills</Link>
+        </Navbar>
+
+        <div className={styles.main_body}>
+          <div className={styles.introbox}>
+            <img
+              className="m-2"
+              src="https://bishantadhikari.vercel.app/assets/img/profile.jpg"
+              alt="My photo"
+              width="200px"
+            />
+            <div className={styles.introDesc}>
+              <div className="head1">
+                Bishant <div className="text-primary">
+                Adhikari
+                </div>
+                </div><br />
+              <h2>MANAMAIJU - 5 KATHMANDU NEPAL <a>BISHANTAD@GMAIL.COM</a></h2><br />
+    
+              Junior developer with experience in Nextjs and Firebase, as well
+              as skills in Python, JavaScript, and Database Management.
+              Proficient in computer science fundamentals and core mathematical
+              concepts such as Linear Algebra, Statistics, and Probabilities.
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
